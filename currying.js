a38f0feaa35323ffa7c3b691ce1284864ea49c61
srinath <srinmolugu@gmail.com>
2024-04-18 00:10:33 +0530

    currying concept

--- currying.js
+++ currying.js
@@ -3,3 +3,15 @@ function addFunc(a, b, c) {
     return a + b + c;
 }
 console.log('output:',addFunc(1,2,3)); //output: 6
+
+//currying
+//currying transforms a function with multiple arguments into a sequence/series of functions, each taking a single argument
+function addFuncCurrying(a) {
+    return (b) => {
+        return (c) => {
+            return a + b + c
+        }
+    }
+}
+
+console.log('currying output:',addFuncCurrying(1)(2)(3))
